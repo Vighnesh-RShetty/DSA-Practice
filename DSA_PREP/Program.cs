a64164c60c74d48df47b4908d7136c879d143e66
VIGGU13 <140583185+VIGGU13@users.noreply.github.com>
2026-02-24 22:58:11 +0530

    Brute Force For The RotateByk

--- DSA_PREP/Program.cs
+++ DSA_PREP/Program.cs
@@ -16,17 +16,26 @@ public class Program
         //Boolean isSorted = ArraySorted.SortedArray(arr);
         //  Boolean isSorted = ArraySorted.SortedArrayOptimal(arr);
         //Console.WriteLine(isSorted ? "Sorted Array" : "Not Sorted Array");
-         int[] arr = { 1, 1, 2,2,3,4,5,6,6,7,8,8,9,9,10,11,11 };
+        //int[] arr = { 1, 1, 2,2,3,4,5,6,6,7,8,8,9,9,10,11,11 };
         //int[] arr = { 1, 2, 2 };
         // ArraySorted.RemoveDublicateEle(arr);
         //int length = ArraySorted.OptimalRemoveDublicate(arr);
         //RotateAnArrayByOnePlace.RotateAnArray(arr);
-        MainClass.LiskovMain(args);
-        
+        //MainClass.LiskovMain(args);
+
         //Console.WriteLine("Array After Moving The Elements");
-       // for(int i = 0; i < arr.Length; i++)
+        // for(int i = 0; i < arr.Length; i++)
+        //{
+        //   Console.Write(" " + arr[i]);
+        // }
+
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7,8,9};
+        int k = 2;
+        RotateAnArrayByKPlace.RotateArray(arr, 3);
+        for (int i = 0; i < arr.Length; i++)
         {
-         //   Console.Write(" " + arr[i]);
+            Console.Write(" " + arr[i]);
         }
+
     }
 }
---
+++ DSA_PREP/VIghnesh/Arrays/RotateAnArrayByKPlace.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA_PREP.VIghnesh.Arrays
+{
+    public class RotateAnArrayByKPlace
+    {
+        
+        public static void RotateArray(int[] arr,int k)
+        {
+            
+
+            if (k == 0 || k == arr.Length || arr.Length==0)
+            {
+                return;
+            }
+            int count = 0;
+
+            k = k % arr.Length;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (k != count)
+                {
+                    int j = 1;
+                    int temp = arr[0];
+                    while (j < arr.Length)
+                    {
+                        arr[j - 1] = arr[j];
+                        j++;
+                    }
+                    arr[arr.Length - 1] = temp;
+                    count++;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
